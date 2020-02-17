3e6e63f216d44525610171c8ac1ff644261e8fe5
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-02-17 14:38:07 -0600

    updated

--- src/CoffeeApp.java
+++ src/CoffeeApp.java
@@ -3,20 +3,23 @@ import java.util.ArrayList;
 public class CoffeeApp {
 
    public ArrayList<Coffee> coffeeList;
-    public CoffeeApp(ArrayList<Coffee> coffeeList) {
-        this.coffeeList = coffeeList;
+
+
+
+    public CoffeeApp() {
+        coffeeList = new ArrayList<>();
     }
 
     Coffee first = new Coffee("french", "light");
     Coffee second = new Coffee("cappuccino", "medium");
     Coffee third = new Coffee("kenyan", "dark");
 
-    public static String addCoffee() {
-
+    public void addCoffee(Coffee newCoffee) {
+       this.coffeeList.add(newCoffee);
     }
 
-    public static String removeCoffee() {
-
+    public void removeCoffee(Coffee coffeeGone) {
+     this.coffeeList.remove(coffeeGone);
     }
 
 }
