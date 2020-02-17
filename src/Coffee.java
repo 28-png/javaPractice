1522ec64730f2d49a44d0915c35da9412843bedc
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-02-17 14:16:18 -0600

    updated

--- src/Coffee.java
+++ src/Coffee.java
@@ -1,5 +1,37 @@
+import java.util.ArrayList;
+
 public class Coffee {
 
+    private String coffeeName;
+    private String coffeeBlend;
 
+    public Coffee(String coffeeName, String coffeeBlend) {
+        this.coffeeName = coffeeName;
+        this.coffeeBlend = coffeeBlend;
+
+
+    }
+
+
+
+    public String getCoffeeName() {
+        return this.coffeeName;
+    }
+
+    public void setCoffeeName(String coffeeName) {
+        this.coffeeName = coffeeName;
+    }
+
+    public String getCoffeeBlend() {
+        return this.coffeeBlend;
+    }
+
+    public void setCoffeeBlend(String coffeeBlend) {
+        this.coffeeBlend = coffeeBlend;
+    }
+
+    public static void addCoffee() {
+
+    }
 
 }
